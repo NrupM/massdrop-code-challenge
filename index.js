09855bdabda566f748d2a7374de7733c9ad6a85c
NrupM <maryenorthrup@gmail.com>
2019-01-24 00:55:42 -0800

    last update to challenge

--- index.js
+++ index.js
@@ -9,71 +9,71 @@ app.use(cors());
 
 mongoose.connect('mongodb://localhost:27017/scrape');
 mongoose.connection
-	.once('open', function() {
-		console.log('Connection has been made, now make fireworks...');
-	})
-	.on('error', function(error) {
-		console.log('Connection error:', error);
-	});
+  .once('open', function() {
+    console.log('Connection has been made, now make fireworks...');
+  })
+  .on('error', function(error) {
+    console.log('Connection error:', error);
+  });
 
-app.get('/scrape', (req, res) => {
-	// get url request parameter
-	const urlParam = req.query.urlParam || '';
+app.get('/butt', (req, res) => {
+  // get url request parameter
+  const urlParam = req.query.urlParam || '';
 
-	if (urlParam === '') {
-		res.status(500).send('required query parameter `URL` missing');
-	}
+  if (urlParam === '') {
+    res.status(500).send('required query parameter `URL` missing');
+  }
 
-	console.log('url parameter passed', urlParam);
-	request(urlParam, (err, response, html) => {
-		if (!response || response.statusCode > 299) {
-			res.status(500).json({
-				message: `failed to scrape url ${urlParam}`,
-				error: err || {},
-				response: response || ''
-			});
-		} else {
-			console.log(err, response, html);
-			const newJob = {
-				url: urlParam,
-				html: html
-			};
+  console.log('url parameter passed', urlParam);
+  request(urlParam, (err, response, html) => {
+    if (!response || response.statusCode > 299) {
+      res.status(500).json({
+        message: `failed to scrape url ${urlParam}`,
+        error: err || {},
+        response: response || ''
+      });
+    } else {
+      console.log(err, response, html);
+      const newJob = {
+        url: urlParam,
+        html: html
+      };
 
-			Job.create(newJob)
-				.then(createdJob => {
-					res.status(200).json(createdJob);
-				})
-				.catch(err => {
-					res.status(500).json({
-						message: `failed to create job ${newJob}`,
-						error: err || {}
-					});
-				});
-		}
-	});
+      Job.create(newJob)
+        .then(createdJob => {
+          res.status(200).json(createdJob);
+        })
+        .catch(err => {
+          res.status(500).json({
+            message: `failed to create job ${newJob}`,
+            error: err || {}
+          });
+        });
+    }
+  });
 });
 
 app.get('/getJobById', (req, res) => {
-	// get job Id request parameter
-	const jobId = req.query.jobId || '';
+  // get job Id request parameter
+  const jobId = req.query.jobId || '';
 
-	if (jobId === '') {
-		res.status(500).send('required query parameter `jobId` missing');
-	}
+  if (jobId === '') {
+    res.status(500).send('required query parameter `jobId` missing');
+  }
 
-	Job.find({ _id: jobId })
-		.then(foundJob => {
-			res.status(200).json(foundJob);
-		})
-		.catch(err => {
-			res.status(500).json({
-				message: `failed to find job with Id ${jobId}`,
-				error: err || {}
-			});
-		});
+  Job.find({ _id: jobId })
+    .then(foundJob => {
+      res.status(200).json(foundJob);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: `failed to find job with Id ${jobId}`,
+        error: err || {}
+      });
+    });
 });
 
 //SERVER
 app.listen(process.env.port || 3000, () =>
-	console.log('http server running on localhost:3000')
+  console.log('http server running on localhost:3000')
 );
